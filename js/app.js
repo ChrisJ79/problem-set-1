e33e4e1dc7b1ce71723224a6a5e51db016e68498
Chris Johnson <csjohnson79@icloud.com>
2016-10-05 14:54:41 -0600

    saved...

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-$
+
+
+
+// Problem Set #1
+// Requirements:
+//
+        // Write a function called letterCapitalize which takes a single string parameter and capitalizes the first letter of each word in the string. You can assume that words in the input string will be separated by only one space.
+
+        var letterCapitalize = function(str) {
+          str = "I am a string!";
+          str.charAt(0);
+        }
+
+
+        // Write a function called wordCount which takes a single string parameter and returns the number of words the string contains (ie. "Never eat shredded wheat" would return 4). You can assume that words in the input string will be separated by only one space.
+
+        // Bonus: Write a function called primeTime that takes a single number parameter and returns true if the parameter is a prime number, otherwise false. The range will be between 1 and 216.
